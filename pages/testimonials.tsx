e857b058b64e938e1a20e2ef9749134f41d588fd
Amit Kumar Dubey <guttume@gmail.com>
2022-07-22 21:25:04 +0530

    optimized for mobile view

--- pages/testimonials.tsx
+++ pages/testimonials.tsx
@@ -13,12 +13,12 @@ export default function Testimonials() {
             className="w-[840px]"
           />
         </div>
-        <div className="px-12">
-          <h1 className="text-5xl font-bold text-darkerText text-center mt-16 w-full max-w-6xl mx-auto">
+        <div className="px-8 lg:px-12">
+          <h1 className="text-3xl lg:text-5xl font-bold text-darkerText text-center mt-16 w-full max-w-6xl mx-auto">
             Loyal Bytes has helped more than 30,000 students across the world to
             achieve their dreams
           </h1>
-          <div className="flex items-center space-x-4 mt-16">
+          <div className="lg:flex items-center lg:space-x-4 space-y-4 lg:space-y-0 mt-16">
             <div className="w-1/8 flex-1">
               <span className="w-full h-[290px] block bg-amber-500 pt-4 rounded-lg">
                 <img
@@ -28,7 +28,7 @@ export default function Testimonials() {
                 />
               </span>
             </div>
-            <div className="w-2/8 flex-1 space-y-4">
+            <div className="lg:w-2/8 flex items-center space-x-4 lg:space-x-0 lg:block flex-1 lg:space-y-4">
               <span className="w-full h-[200px] block bg-beigeDark pt-4 rounded-lg">
                 <img
                   src={images.testimonials.grids.two}
@@ -53,7 +53,7 @@ export default function Testimonials() {
                 />
               </span>
             </div>
-            <div className="w-2/8 flex-1 space-y-4">
+            <div className="lg:w-2/8 flex lg:block items-center space-x-4 lg:space-x-0 flex-1 lg:space-y-4">
               <span className="w-full h-[200px] block bg-orange-500 pt-4 rounded-lg">
                 <img
                   src={images.testimonials.grids.five}
@@ -81,7 +81,7 @@ export default function Testimonials() {
           </div>
         </div>
       </section>
-      <section className="px-12 mt-32">
+      <section className="px-8 lg:px-12 mt-16 lg:mt-32">
         <h3 className="text-5xl font-bold text-darkerText text-center">
           What Our Students Say About Us
         </h3>
@@ -89,7 +89,7 @@ export default function Testimonials() {
           <img src={images.testimonials.bar} alt="A picture of a bar" />
         </div>
         <div className="space-y-8">
-          <div className="flex space-x-4">
+          <div className="lg:flex lg:space-x-4">
             <div>
               <TestimonialItem
                 imageUrl={images.sliderImageOne}
@@ -121,7 +121,7 @@ export default function Testimonials() {
               </TestimonialItem>
             </div>
           </div>
-          <div className="flex space-x-4">
+          <div className="lg:flex lg:space-x-4">
             <div>
               <TestimonialItem
                 imageUrl={images.sliderImageTwo}
@@ -153,7 +153,7 @@ export default function Testimonials() {
               </TestimonialItem>
             </div>
           </div>
-          <div className="flex space-x-4">
+          <div className="lg:flex lg:space-x-4">
             <div>
               <TestimonialItem
                 imageUrl={images.sliderImageOne}
@@ -185,7 +185,7 @@ export default function Testimonials() {
               </TestimonialItem>
             </div>
           </div>
-          <div className="flex space-x-4">
+          <div className="lg:flex lg:space-x-4">
             <div>
               <TestimonialItem
                 imageUrl={images.sliderImageTwo}
