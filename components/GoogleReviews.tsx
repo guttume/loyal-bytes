f60074924a60e8a62f73c1479122aaae10c3e50f
Amit Kumar Dubey <guttume@gmail.com>
2022-08-06 15:55:59 +0530

    no referefer

--- components/GoogleReviews.tsx
+++ components/GoogleReviews.tsx
@@ -6,6 +6,7 @@ export const GoogleReviews = () => {
       className="btn btn-primary mb-4 flex items-center space-x-2"
       href="https://www.google.com/search?q=loyal+bytes+global+it+services&rlz=1C1CHZN_enIN988IN988&oq=loyal+bytes+globa&aqs=chrome.0.35i39j46i175i199i512j69i57j0i390l3j69i64j69i60.6034j0j7&sourceid=chrome&ie=UTF-8#cobssid=s&lrd=0x3be7c86cc2e6ce63:0x9cbe8c2b76f6cf4e,1"
       target={"_blank"}
+      rel="noreferrer"
     >
       <span>Google Reviews</span>
       <span>
