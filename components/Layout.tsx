2857a5b897e78e10cf3b90409469d0162e3edcd7
Amit Kumar Dubey <guttume@gmail.com>
2022-08-11 03:54:57 +0530

    added cookie consent

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import { PropsWithChildren } from "react";
+import { CookieConsent } from "react-cookie-consent";
 import { Footer } from "../layouts/Footer";
 import { Header } from "./Header";
 
@@ -8,6 +9,18 @@ export const Layout = ({ children }: PropsWithChildren) => {
       <Header />
       <main>{children}</main>
       <Footer />
+      <CookieConsent
+        location="bottom"
+        buttonText="OK"
+        cookieName="LBCookieConsent"
+        style={{ background: "#2B373B" }}
+        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+        expires={150}
+      >
+        We use cookies and other tracking technologies to improve your browsing
+        experience on our website and targeted ads, to analyze our website
+        traffic, and to understand where our visitors are coming from.
+      </CookieConsent>
     </div>
   );
 };
