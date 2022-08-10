d8551e5fe350b3c710c313ab1946929186699fab
Amit Kumar Dubey <guttume@gmail.com>
2022-08-11 00:57:02 +0530

    added animate css

--- tailwind.config.js
+++ tailwind.config.js
@@ -64,5 +64,17 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/line-clamp")],
+  plugins: [require("@tailwindcss/line-clamp"),
+  require('tailwindcss-animatecss')({
+    classes: ['animate__animated', 'animate__tada'],
+    settings: {
+      animatedSpeed: 1000,
+      heartBeatSpeed: 1000,
+      hingeSpeed: 2000,
+      bounceInSpeed: 750,
+      bounceOutSpeed: 750,
+      animationDelaySpeed: 1000
+    },
+    variants: ['responsive', 'hover', 'reduced-motion'],
+  }),],
 };
