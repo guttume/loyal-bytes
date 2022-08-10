9856ecb4f43216fe13aa8624004648c6494890ab
Amit Kumar Dubey <guttume@gmail.com>
2022-08-11 00:52:17 +0530

    remove overflow clip from body

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
           rel="stylesheet"
         ></link>
       </Head>
-      <body style={{overflowX: 'clip'}}>
+      <body>
         <Main />
         <NextScript />
       </body>
