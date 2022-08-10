b195aba235be7a761574d25101f4bc2af5e9ca20
Amit Kumar Dubey <guttume@gmail.com>
2022-08-11 03:51:17 +0530

    fixed contacts page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -5,12 +5,12 @@ export default function Contact() {
   return (
     <>
       <section className="p-8 lg:p-12">
-        <div className="lg:flex items-center lg:space-x-2">
+        <div className="items-center lg:flex lg:space-x-2">
           <div className="lg:pl-20 lg:w-7/12">
-            <h1 className="text-4xl lg:text-7xl font-bold text-darkerText">
+            <h1 className="text-4xl font-bold lg:text-7xl text-darkerText">
               Become a part of a Global Community
             </h1>
-            <ul className="mt-8 lg:mt-16 text-xl border-l-2 border-orange-500 pl-4">
+            <ul className="pl-4 mt-8 border-l-2 border-orange-500 md:text-xl lg:mt-16">
               {[
                 "Virtual classes available",
                 "Money back guarantee",
@@ -18,17 +18,17 @@ export default function Contact() {
               ].map((item, index) => (
                 <li
                   key={index}
-                  className="flex items-center justify-between w-72"
+                  className="flex items-center justify-between w-56 md:w-72"
                 >
                   <span>{item}</span>
                   <span>
-                    <CheckIcon className="h-4 w-4 text-green-500" />
+                    <CheckIcon className="w-4 h-4 text-green-500" />
                   </span>
                 </li>
               ))}
             </ul>
           </div>
-          <div className="lg:w-5/12 mt-8 lg:mt-0">
+          <div className="mt-8 lg:w-5/12 lg:mt-0">
             <img
               src={images.contact.hero}
               alt="A picture of a boy having coffee"
@@ -40,27 +40,27 @@ export default function Contact() {
         <Statistics />
       </section>
       <section className="py-8 lg:p-24">
-        <div className="w-full bg-gray-200 lg:rounded-lg p-8 lg:p-20 space-y-4 lg:space-y-20">
-          <div className="w-full lg:flex lg:space-x-8 items-center">
-            <div className="lg:w-1/3 space-y-4">
+        <div className="w-full p-8 space-y-4 bg-gray-200 lg:rounded-lg lg:p-20 lg:space-y-20">
+          <div className="items-center w-full lg:flex lg:space-x-8">
+            <div className="space-y-4 lg:w-1/3">
               <h3 className="text-3xl font-bold text-darkerText">
                 Have a query?
               </h3>
-              <h4 className="text-lg font-bold text-gray-600 pb-4 lg:pb-12">
+              <h4 className="pb-4 text-lg font-bold text-gray-600 lg:pb-12">
                 Get in touch with us and our friendly and professional staff
                 will guide you step by step to resolve your queries.
               </h4>
               <a
-                className="flex space-x-4 items-center btn btn-outline-primary w-64 justify-center"
+                className="flex items-center justify-center w-64 space-x-4 btn btn-outline-primary"
                 href="https://forms.office.com/r/VEQqdjwzZr"
               >
                 <span>Get in touch</span>
                 <span>
-                  <ArrowRightIcon className="h-5 w-5 text-orange-500" />
+                  <ArrowRightIcon className="w-5 h-5 text-orange-500" />
                 </span>
               </a>
             </div>
-            <div className="lg:w-2/3 mt-4 lg:mt-0">
+            <div className="mt-4 lg:w-2/3 lg:mt-0">
               <img
                 src={images.contact.map}
                 alt="A picture of a google map"
@@ -68,8 +68,8 @@ export default function Contact() {
               />
             </div>
           </div>
-          <div className="lg:flex space-y-6 lg:space-y-0 justify-between items-center">
-            <div className="flex space-x-2 items-center lg:w-1/3">
+          <div className="items-center justify-between space-y-6 lg:flex lg:space-y-0">
+            <div className="flex items-center space-x-2 lg:w-1/3">
               <span>
                 <img
                   src={images.contact.icons.phone}
@@ -81,7 +81,7 @@ export default function Contact() {
                 <a href="">+91 9769303625</a>
               </span>
             </div>
-            <div className="flex space-x-2 items-center lg:w-1/3">
+            <div className="flex items-center space-x-2 lg:w-1/3">
               <span>
                 <img
                   src={images.contact.icons.mail}
@@ -94,7 +94,7 @@ export default function Contact() {
                 <a href="">rajesh.yadav@loyalbytes.com</a>
               </span>
             </div>
-            <div className="flex space-x-2 items-center lg:w-1/3">
+            <div className="flex items-center space-x-2 lg:w-1/3">
               <span>
                 <img
                   src={images.contact.icons.location}
@@ -111,7 +111,7 @@ export default function Contact() {
           </div>
         </div>
       </section>
-      <section className="lg:px-16 mb-16">
+      <section className="px-8 mb-16 lg:px-16">
         <HurryUp />
       </section>
     </>
