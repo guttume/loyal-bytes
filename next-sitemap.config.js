8b26c57ae4bee8800086043d4ad55330ea611fcd
Amit Kumar Dubey <guttume@gmail.com>
2022-10-18 11:16:20 +0530

    add .html to sitemap links

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,6 +3,24 @@ module.exports = {
     siteUrl: process.env.SITE_URL || 'https://learning.loyalbytes.com',
     generateRobotsTxt: true, // (optional)
     generateIndexSitemap: false,
-    outDir: "out"
+    outDir: "out",
+    transform: async (config, path) => {
+        if (path !== '/') {
+            path = `${path}.html`
+        }
+
+        if (path === '/course/windows-server-hybrid-administrator-associate-az-800-&-az-801')
+        {
+            path = `/course/windows-server-hybrid-administrator-associate-az-800-&-az-801.html`
+        }
+
+        return {
+            loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+            changefreq: config.changefreq,
+            priority: config.priority,
+            lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+            alternateRefs: config.alternateRefs ?? [],
+        }
+    }
     // ...other options
 }
